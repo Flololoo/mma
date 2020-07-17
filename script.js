caea290d3536d9968864ac073f8ec2e2742afebc
Flo Muellner <florian.muellner95@gmail.com>
2020-07-17 09:24:25 +0200

    ~ changed basically everything. changed to dark theme

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ $('.js-contact-button').on('click', function scrollToContactForm () {
         scrollTop: $($(".js-form")[0]).offset().top
     }, 2000);
 })
+
+
+$('.js-fading').on('click', function showElement() {
+    console.log($(e.target));
+})
+
+
+
+
+
+let $blocks = $('.js-fading');
+scrollContent($blocks);
+document.addEventListener('scroll', function (event) {
+    scrollContent($blocks);
+}, true /*Ca    pture event*/);
+
+function scrollContent($blocks) {
+    let screenBottom = $(window).scrollTop() + $(window).height();
+
+    $blocks.each((index) => {
+        if (screenBottom < $($blocks[index]).offset().top) {
+            $($blocks[index]).css('transform', 'translateX(-300%)');
+            $($blocks[index]).css('opacity', '0');
+        }else {
+            $($blocks[index]).css('opacity', '1');
+            $($blocks[index]).css('transform', 'translateX(0)');
+
+        }
+    });
+}
